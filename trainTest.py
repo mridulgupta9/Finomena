d3cfd1bea2fcdc6d7fb589e5bb9ee11beff6468e
mridulgupta9 <mridulgupta9@gmail.com>
2016-04-17 22:29:14 +0530

    Update trainTest.py

--- trainTest.py
+++ trainTest.py
@@ -10,6 +10,17 @@ clf.fit(X, y)
 _ = joblib.dump(clf, "traineddata.pkl")
 clf = joblib.load("traineddata.pkl")
 #data_test = np.loadtxt('testdata.csv', delimiter=',')
-anslist=list(clf.predict(data_train[length:-1,:]))
-c = csv.writer(open("submission_new.csv", "wb"))
+anslist=list(clf.predict(data_test[length:-1,1:]))
+actualans=[]
+k=0
+for i in range(len(anslist)):
+	actualans.append(data_test[length+i,0])
+	if anslist[i]==data_test[length+i,0]:
+		k+=1
+print k*100.0/len(anslist)
+
+
+c = csv.writer(open("submission.csv", "wb"))
+
 c.writerow(anslist)
+c.writerow(actualans)
